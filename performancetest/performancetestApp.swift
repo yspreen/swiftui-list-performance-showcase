41b515c8bd22216d3b9a8b47c81992334f43c4d7
Yannick <yannick.spreen@gmail.com>
2023-01-03 17:38:07 -0500

    Initial Commit

---
+++ performancetest/performancetestApp.swift
@@ -0,0 +1,17 @@
+//
+//  performancetestApp.swift
+//  performancetest
+//
+//  Created by Nick on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct performancetestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
